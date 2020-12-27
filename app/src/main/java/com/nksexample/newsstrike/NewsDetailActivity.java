f00d9fbeaa6d8f6ac3df32fdba1f549a04f4563e
Neoh Kim Seng <neohks21@gmail.com>
2020-12-27 18:42:06 +0800

    + Add NewsDetailActivity

--- app/src/main/java/com/nksexample/newsstrike/NewsDetailActivity.java
+++ app/src/main/java/com/nksexample/newsstrike/NewsDetailActivity.java
@@ -1,23 +1,181 @@
 package com.nksexample.newsstrike;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class NewsDetailActivity extends AppCompatActivity {
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 
-    TextView tvNewsDetailTitle;
+public class NewsDetailActivity extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener {
+
+    private ImageView imageView;
+    private TextView tvAppbarTitle, tvAppbarSubtitle, tvDate, tvTime, tvTitle;
+    private boolean isHideToolbarView = false;
+    private FrameLayout date_behavior;
+    private LinearLayout titleAppbar;
+    private AppBarLayout appBarLayout;
+    private Toolbar toolbar;
+    private String gUrl, gImg, gTitle, gDate, gSource, gAuthor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
 
-        tvNewsDetailTitle = findViewById(R.id.tvNewsDetailTitle);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // Get Ectras
-        tvNewsDetailTitle.setText("");
+        final CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle("");
+
+        //Associations
+        appBarLayout = findViewById(R.id.appbar);
+        appBarLayout.addOnOffsetChangedListener(this);
+        date_behavior = findViewById(R.id.date_behavior);
+        titleAppbar = findViewById(R.id.title_appbar);
+        imageView = findViewById(R.id.backdrop);
+        tvAppbarTitle = findViewById(R.id.tvAppbarTitle);
+        tvAppbarSubtitle = findViewById(R.id.tvAppbarSubtitle);
+        tvDate = findViewById(R.id.tvDate);
+        tvTime = findViewById(R.id.tvTime);
+        tvTitle = findViewById(R.id.tvTitle);
+
+        // Get Extras
+        Intent intent = getIntent();
+        gUrl = intent.getStringExtra("url");
+        gImg = intent.getStringExtra("img");
+        gTitle = intent.getStringExtra("title");
+        gDate = intent.getStringExtra("date");
+        gSource = intent.getStringExtra("source");
+        gAuthor = intent.getStringExtra("author");
+
+        RequestOptions requestOptions = new RequestOptions();
+        requestOptions.error(Utils.getRandomDrawbleColor());
+
+        Glide.with(this)
+                .load(gImg)
+                .apply(requestOptions)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(imageView);
+
+        tvAppbarTitle.setText(gSource);
+        tvAppbarSubtitle.setText(gUrl);
+        tvDate.setText(Utils.DateFormat(gDate));
+        tvTitle.setText(gTitle);
+
+        String author;
+        if (gAuthor != null){
+            author = " \u2022 " + gAuthor;
+        } else {
+            author = "";
+        }
+
+        tvTime.setText(gSource + author + " \u2022 " + Utils.DateToTimeFormat(gDate));
+
+        initWebView(gUrl);
 
     }
+
+    private void initWebView(String gUrl) {
+
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(gUrl);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        supportFinishAfterTransition();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();;
+        return true;
+    }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+
+        int maxScroll = appBarLayout.getTotalScrollRange();
+        float percentage = (float) Math.abs(verticalOffset) / (float) maxScroll;
+
+        if (percentage == 1f && isHideToolbarView) {
+            date_behavior.setVisibility(View.GONE);
+            titleAppbar.setVisibility(View.VISIBLE);
+            isHideToolbarView = !isHideToolbarView;
+
+        } else if (percentage < 1f && !isHideToolbarView) {
+            date_behavior.setVisibility(View.VISIBLE);
+            titleAppbar.setVisibility(View.GONE);
+            isHideToolbarView = !isHideToolbarView;
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_news_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.view_web){
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(gUrl));
+            startActivity(i);
+            return true;
+        }
+
+        else if (id == R.id.share){
+            try{
+
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("text/plan");
+                i.putExtra(Intent.EXTRA_SUBJECT, gSource);
+                String body = gTitle + "\n" + gUrl + "\n" + "Share from the News App" + "\n";
+                i.putExtra(Intent.EXTRA_TEXT, body);
+                startActivity(Intent.createChooser(i, "Share with :"));
+
+            }catch (Exception e){
+                Toast.makeText(this, "Hmm.. Sorry, \nCannot be share", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
